05561dc3fa8fc1bc50fb654d3df70e046aefe902
newwangzhicheng <newwangzhicheng@hotmail.com>
2024-03-29 00:57:04 +0800

    add rush prettier

--- apps/upload-file-client/app/page.tsx
+++ apps/upload-file-client/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 
 export default function Home() {
-  const a = 'hello world';
+  const a = 'hello world'
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <label htmlFor="uploader">
         上传文件
-        <input type="file" name={'uploader'}/>
+        <input type="file" name={'uploader'} />
       </label>
     </main>
   )
