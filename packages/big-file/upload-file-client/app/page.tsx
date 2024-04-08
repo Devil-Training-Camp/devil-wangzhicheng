0d60b3330745c73d6109912dee83fd3f9af892ed
newwangzhicheng <newwangzhicheng@hotmail.com>
2024-04-08 08:17:12 +0800

    改变位置

--- packages/big-file/upload-file-client/app/page.tsx
+++ packages/big-file/upload-file-client/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <label htmlFor="uploader">
-        上传文件
         <input type="file" name={'uploader'} />
       </label>
     </main>
