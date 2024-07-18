34e00173ffa6f6d6bec9d7d9ae09484d68e7f25b
newwangzhicheng <newwangzhicheng@hotmail.com>
2024-07-19 03:50:24 +0800

    create web

---
+++ packages/dep-graph/web/src/App.tsx
@@ -0,0 +1,9 @@
+import ForceGraph from './view/ForceGraph.tsx'
+
+export default function App() {
+  return (
+    <div className="w-full h-full">
+      <ForceGraph />
+    </div>
+  )
+}
---
+++ packages/dep-graph/web/src/view/ForceGraph.tsx
@@ -0,0 +1,17 @@
+import ForceGraph3D from 'react-force-graph-3d'
+
+function genRandomTree(N = 300, reverse = false) {
+  return {
+    nodes: [...Array(N).keys()].map((i) => ({ id: i })),
+    links: [...Array(N).keys()]
+      .filter((id) => id)
+      .map((id) => ({
+        [reverse ? 'target' : 'source']: id,
+        [reverse ? 'source' : 'target']: Math.round(Math.random() * (id - 1))
+      }))
+  }
+}
+
+export default function ForceGraph() {
+  return <ForceGraph3D graphData={genRandomTree()} />
+}
