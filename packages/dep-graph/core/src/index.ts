b7bb96327448f5fcbf60ab44dc10132ce78ea286
wangzhicheng <514046413@qq.com>
2024-07-24 23:58:39 +0800

    cli/web

--- packages/dep-graph/core/src/index.ts
+++ packages/dep-graph/core/src/index.ts
@@ -1,8 +1,7 @@
 import PackageParser from './PackageParser'
 import PnpmPackageParser from './PnpmPackageParser'
 
-export * from './PackageParser'
-export * from './PnpmPackageParser'
+export { Node, Link, Dependencies } from './PackageParser'
 
 export default {
   PackageParser,
