ca488de8a4be80d9ce35a6ac70ffee8bb1c2e0a3
newwangzhicheng <newwangzhicheng@hotmail.com>
2025-01-29 10:22:09 +0800

    ts-config

---
+++ packages/dep-graph/web/scripts/bootstrap.ts
@@ -0,0 +1,15 @@
+// import {Dependencies} from "@dep-graph/core";
+// import {createServer} from "vite";
+// import {fileURLToPath} from "node:url";
+//
+// interface Params {
+//   dependencies: Dependencies
+// }
+
+// const __dirname = fileURLToPath(new URL('.', import.meta.url))
+//
+// export const drawGraph = async (params: Params) {
+//   const server = createServer({
+//
+//   })
+// }
